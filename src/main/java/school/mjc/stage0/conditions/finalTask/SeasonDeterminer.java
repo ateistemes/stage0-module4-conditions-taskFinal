d54b6b69a54269a44d74ae93c5ccae174a33f992
ateistemes <timur.tynychbekov@gmail.com>
2023-10-14 21:35:00 +0600

    t

--- src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
@@ -2,5 +2,20 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class SeasonDeterminer {
     public void tellTheSeason(int monthNumber) {
+        if( monthNumber < 1 || monthNumber > 12){
+            System.out.println("Wrong month number");
+        }else{
+            if( monthNumber >= 3 && monthNumber <= 5){
+                System.out.println("Spring");
+            } else if (monthNumber >= 6 && monthNumber <= 8) {
+                System.out.println("Summer");
+
+            } else if (monthNumber >= 9 && monthNumber <= 11) {
+                System.out.println("Fall");
+
+            }else{
+                System.out.println("Winter");
+            }
+        }
     }
 }
